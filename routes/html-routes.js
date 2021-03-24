fb0c2c69d0ac22e3e3c7bc3c4f6335185bde3b87
Nick Vandenberge <nvandenberge14@gmail.com>
2021-03-24 18:48:56 -0400

    html route for exercise page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,5 +3,9 @@ var path = require("path");
 module.exports = (app) => {
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"))
-    })
+    });
+
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    });
 }
