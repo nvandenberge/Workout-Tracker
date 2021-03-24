bdce3db3e3c7533fe5bb3e0d3345715a05d2503b
Nick Vandenberge <nvandenberge14@gmail.com>
2021-03-24 19:51:43 -0400

    api route to get all workouts

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,1 +1,13 @@
-module.exports = function (app) {}
+const db = require("../models");
+
+module.exports = (app) => {
+    app.get("/api/workouts", (req, res) => {
+        db.Workout.find()
+        .then(workouts => {
+            res.json(workouts)
+        })
+        .catch(error => {
+            res.json(err)
+        })
+    })
+}
