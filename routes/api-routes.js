031d9ae0234a16e9e778e81bcb00e8710c58042b
Nick Vandenberge <nvandenberge14@gmail.com>
2021-03-25 15:03:24 -0400

    added api routes to add workout data to mongo

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,13 +1,39 @@
 const db = require("../models");
 
 module.exports = (app) => {
-    app.get("/api/workouts", (req, res) => {
-        db.Workout.find()
-        .then(workouts => {
-            res.json(workouts)
-        })
-        .catch(error => {
-            res.json(err)
-        })
-    })
-}
+  // GET all workout data when hitting workouts page
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find()
+      .then((workouts) => {
+        res.json(workouts);
+      })
+      .catch((err) => {
+        res.json("error == ", err);
+      });
+  });
+
+  // POST route for when workout is 'completed'
+  app.post("/api/workouts/", (req, res) => {
+    db.Workout.create(req.body)
+      .then((workouts) => {
+        res.json(workouts);
+      })
+      .catch((err) => {
+        console.log("error == ", err);
+      });
+  });
+
+  // PUT route to add/update exercises for current workout
+  app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.findByIdAndUpdate(
+      { _id: req.params.id },
+      { $push: { exercises: req.body } },
+    )
+      .then((workouts) => {
+        res.json(workouts);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+};
